28f21c526e81fafec35c1ddf56aa4f87697019fb
Fabrice Monaco <fabrice.monaco@soprasteria.com>
2022-11-26 23:09:42 +0100

    env prod modif

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
-export const environment = {
-  production: true
+import { Environment } from "../app/core/interfaces/environment";
+
+export const environment:Environment = {
+  production: false,
+  appToken: "5a4b2d457ecbef9eb2a71e480b947604",
+  apiUrl: "http://api.openweathermap.org/data/2.5",
+  iconUrl:   "https://raw.githubusercontent.com/udacity/Sunshine-Version-2/sunshine_master/app/src/main/res/drawable-hdpi/"
 };
